5338fcc3cd79951e961fff85d3d469f631112431
haiconga263 <trandinhhiephtd@gmail.com>
2016-06-10 14:05:57 +0700

    dsa
    ads

--- WIMSystemService/MainWindow.xaml.cs
+++ WIMSystemService/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
-
+using WIMSystemService.Deserializer;
+using WIMSystemService.Process;
 using WIMSystemService.Request;
 using WIMSystemService.Serializer;
 
@@ -23,7 +24,9 @@ namespace WIMSystemService
             //Request_Data rq = new Request_Data { Number = "123", Type = "123", RequestInfor = new Request_Doc { Receiver = "123", Sender = "123", Request = new Sub_Request { SubSystemSpec = new XmlElement.SubsystemInfor { SubSystem = "123", SubSystemType = "231" } } } };
             try
             {
-                MySerializerXML<Request_Resend_Data>.Serialize(rsd, "thai");
+                //MySerializerXML<Request_Resend_Data>.Serialize(rsd, "thai");
+
+                Pro_Alarm_Overweight dt = DeserializerXML<Pro_Alarm_Overweight>.Deserializer("demoxml.xml");
             }
             catch (Exception ex)
             {
---
+++ WIMSystemService/Process/DeserializerXML.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WIMSystemService.Deserializer
+{
+    public class DeserializerXML<T>
+        where T : class, new()
+    {
+        public static T Deserializer(string Name)
+        {
+            using (TextReader reader = new StreamReader(Name))
+            {
+                try
+                {
+                    var xmlSerializer = new XmlSerializer(typeof(T));
+                    T data = xmlSerializer.Deserialize(reader) as T;
+                    return data;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
